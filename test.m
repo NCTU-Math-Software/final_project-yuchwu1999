0b397be8ed51bef74e14edcdff10be48fb653968
yuchwu1999 <71265464+yuchwu1999@users.noreply.github.com>
2020-12-13 14:16:28 +0800

    Update test.m

--- test.m
+++ test.m
@@ -1,7 +1,8 @@
 
+
 N = (2.3).*10^7 ;%total population
 I0 = 100; % initial number of infected
-T = 2000; % period 
+T = 1000; % period 
 beta=zeros(1,T);
 gamma=zeros(1,T);
 
@@ -10,10 +11,8 @@ gamma(1) = 0.0101; %recovery rate
 
 dt = 1; % time interval of 6 hours (1/8 of a day)
 
-fprintf('Value of parameter R0 is %.2f',(N.*beta)./gamma)
-
 % Calculate the model
-[S,I,R] = sir_model(beta,gamma,N,I0,T,dt);
+[S,I,R,beta,gamma] = sir_model(beta,gamma,N,I0,T,dt);
 % Plots that display the epidemic outbreak
 
 tt = 0:dt:T-dt;
@@ -26,10 +25,13 @@ ylabel('Number of individuals');
 
 legend('S','I','R');
 
+for jj=1:T
+fprintf('Value of parameter R0 is %.2f',(N.*beta(jj))./gamma(jj))
+disp(' ')
+end
 
 
-
-function [S,I,R] = sir_model(beta,gamma,N,I0,T,dt)
+function [S,I,R,beta,gamma] = sir_model(beta,gamma,N,I0,T,dt)
    
     S = zeros(1,T/dt);
     S(1) = N;
@@ -37,7 +39,7 @@ function [S,I,R] = sir_model(beta,gamma,N,I0,T,dt)
     I(1) = I0;
     R = zeros(1,T/dt);
     
-    for tt = 1:(T/dt)-1
+    for tt = 1:T-1
         
         dS = (-beta(tt)*I(tt)*S(tt)) * dt;
         dI = (beta(tt)*I(tt)*S(tt) - gamma(tt)*I(tt)) * dt;
