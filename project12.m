ddb58289bde914faf294971b9c122cb180a96e95
yuchwu1999 <71265464+yuchwu1999@users.noreply.github.com>
2020-12-15 10:35:25 +0800

    Update project12.m

--- project12.m
+++ project12.m
@@ -8,7 +8,7 @@ gamma=zeros(1,T);
 beta(1)= 1.*10^(-9); %infection rate
 gamma(1) =0.01; %recovery rate
 
-dt = 1; % time interval of 6 hours
+dt = 1; % time interval
 
 % Calculate the model
 [S,I,R,beta,gamma] = sir_model(beta,gamma,N,I0,T,dt);
@@ -46,7 +46,7 @@ function [S,I,R,beta,gamma] = sir_model(beta,gamma,N,I0,T,dt)
     S(1) = N-I(1);
     for tt = 1:T-1
 
-       gamma(tt+1)=fsolve(@(x) (R(tt+1)-(x*I(tt))-R(tt) * dt),1);
+       gamma(tt+1)=fsolve(@(x) (R(tt+1)-(x*I(tt))*dt-R(tt),1);
        beta(tt+1)=fsolve(@(y) (I(tt+1)-(y*I(tt)*(N-I(tt)-R(tt)) - gamma(tt+1)*I(tt)) * dt-I(tt)),1);
 
 
